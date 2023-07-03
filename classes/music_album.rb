c317260eb4cafac527c5c266193ba91cd9cd2f61
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-03 14:02:34 +0000

    Add 'on_spotify' as an attr_reader

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -1,6 +1,8 @@
 require_relative 'item'
 
 class MusicAlbum < Item
+  attr_reader :on_spotify
+
   def initialize(published_date, archived, on_spotify)
     super(published_date, archived)
     @on_spotify = on_spotify
