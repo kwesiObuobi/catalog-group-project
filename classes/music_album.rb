93c43ecab9c19a3147a13ed097ff8c7aacd212c4
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-07-04 10:20:36 +0000

    Add unit tests for items in Music Album class

--- classes/music_album.rb
+++ classes/music_album.rb
@@ -3,8 +3,8 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_reader :on_spotify
 
-  def initialize(published_date, archived, on_spotify)
-    super(published_date, archived)
+  def initialize(published_date, on_spotify)
+    super(published_date)
     @on_spotify = on_spotify
   end
 
